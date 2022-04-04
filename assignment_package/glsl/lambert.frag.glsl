76e4f45fc3c5d33303a12f893f93942a1d56f77e
zongxinc <zongxinc@bu.edu>
2022-04-04 00:28:29 -0400

    Update lambert.frag.glsl

--- assignment_package/glsl/lambert.frag.glsl
+++ assignment_package/glsl/lambert.frag.glsl
@@ -80,9 +80,9 @@ void main()
         float diffuseTerm = 0;
         if (fs_Nor.w == 0.5){
             diffuseColor = texture(u_texture, fs_UV);
-//            diffuseColor = diffuseColor * (0.5 * fbm(fs_Pos.xyz) + 0.5);
+            diffuseColor.rbg = diffuseColor.rbg * (0.5 * fbm(fs_Pos.xyz) + 0.5);
             vec4 my_Nor = texture(u_normTexture, fs_UV);
-            diffuseTerm = dot(normalize(fs_Nor + my_Nor), normalize(fs_LightVec));
+            diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
         }else{
 
             diffuseColor = texture(u_texture, vec2(fs_UV.x + (u_Time % 100) * 0.01 * 0.0625, fs_UV.y));
