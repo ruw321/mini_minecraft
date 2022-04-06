3a1bdd3476ce8603bd007c93d02dd73192f6112e
zongxinc <zongxinc@bu.edu>
2022-04-06 13:53:20 -0400

    improved fog effect

--- assignment_package/glsl/lambert.frag.glsl
+++ assignment_package/glsl/lambert.frag.glsl
@@ -28,7 +28,7 @@ in vec4 fs_Col;
 out vec4 out_Col; // This is the final output color that you will see on your
                   // screen for the pixel that is currently being processed.
 
-const vec4 fogColor = vec4(0.8, 0.9, 1, 0.9);
+const vec4 fogColor = vec4(0.8, 0.9, 1, 1);
 
 float random1(vec3 p) {
     return fract(sin(dot(p,vec3(127.1, 311.7, 191.999)))
@@ -131,7 +131,7 @@ void main()
         float d = distance(u_camPos, fs_Pos);
         float fogAlpha = smoothstep(30.f, 200.f, d);
 
-        out_Col = mix(vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a), fogColor, fogAlpha);
+        out_Col = mix(vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a), fogColor, fogAlpha * 0.7);
 
 //        out_Col = diffuseColor;
 }
